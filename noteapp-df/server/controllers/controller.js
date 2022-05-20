c6c0993f04bb181614040523355b170c19a18fc7
Julio Vettori <vettoribmxx@gmail.com>
2022-05-20 14:41:35 -0300

    Programa finalizado
    V1.0

--- noteapp-df/server/controllers/controller.js
+++ noteapp-df/server/controllers/controller.js
@@ -1,5 +1,50 @@
-const index = (req,res) => {
-    res.render('index')
+let Notes = []
+
+const _index = (req, res) => {
+    res.render('index', { Notes })
 }
 
-module.exports = { index }
+const _create = (req, res) => {
+    res.render('create')
+}
+
+const _edit = (req, res) => {
+    const founded = Notes.find((note) => note.id == req.params.id)
+    res.render('edit', { founded })
+}
+
+const _createNote = (req, res) => {
+    const newNote = {
+        id: Date.now(),
+        title: req.body.title,
+        content: req.body.content
+    }
+
+    Notes.push(newNote)
+
+    res.redirect('/')
+}
+
+const _editNote = (req, res) => {
+    const { title, content } = req.body
+
+    const noteToEdit = Notes.find((note) => note.id == req.params.id)
+
+    if (title){
+        noteToEdit.title = title
+    }
+
+    if (content){
+        noteToEdit.content = content
+    }
+
+    res.redirect('/')    
+}
+
+const _deleteNote = (req, res) => {
+    Notes = Notes.filter((note) => note.id != req.params.id)
+    
+    res.redirect('/')
+}
+
+module.exports = { _index, _create, _edit, _createNote, _editNote, _deleteNote }
--- noteapp-df/server/routes/route.js
+++ noteapp-df/server/routes/route.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const route = express.Router()
-const { index } = require('../controllers/controller')
+const { _index, _create, _edit, _createNote, _editNote, _deleteNote } = require('../controllers/controller')
 
-route.get('/', index)
+route.get('/', _index)
+route.get('/create', _create)
+route.get('/edit/:id', _edit)
+
+route.post('/create', _createNote)
+route.put('/edit/:id', _editNote)
+route.delete('/delete/:id', _deleteNote)
+
 
 module.exports = route
